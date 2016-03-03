4b112b547d4a1840aeb820f35c688f918ae8fd64
ajnolte12 <ajnolte12@gmail.com>
2016-03-03 17:34:29 -0600

    Add Button Listeners

--- app/src/main/java/com/example/adam/android_arduino_bluetooth/MainActivity.java
+++ app/src/main/java/com/example/adam/android_arduino_bluetooth/MainActivity.java
@@ -8,13 +8,48 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int signalStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signalStatus = 0;
+
+        Switch onOffSwitch = (Switch) findViewById(R.id.onoffswitch);
+        onOffSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // do something, the isChecked will be
+                // true if the switch is in the On position
+                if(isChecked){
+                    setOn();
+                }
+                else{
+                    setOff();
+                }
+            }
+        });
+
+        RadioGroup highLow = (RadioGroup) findViewById(R.id.highLowRadio);
+        highLow.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.highButton){
+                    setHigh();
+                }
+                else{
+                    setLow();
+                }
+            }
+        });
     }
 
     @Override
@@ -37,5 +72,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void setOn(){
+        System.out.println("Sending On Signal");
+    }
+
+    public void setOff(){
+        System.out.println("Sending Off Signal");
+    }
+
+    public void setHigh(){
+        System.out.println("Setting Low");
+    }
+
+    public void setLow(){
+        System.out.println("Setting High");
+    }
+
+    public void sendSignal(){
+
     }
 }
