e44cf2375f0037bd12cf3ec0d57836f477a701c2
ajnolte12 <ajnolte12@gmail.com>
2016-03-03 18:27:04 -0600

    Rename Package

--- app/src/main/java/com/ece3110/adam/android_arduino_bluetooth/MainActivity.java
+++ app/src/main/java/com/ece3110/adam/android_arduino_bluetooth/MainActivity.java
@@ -1,17 +1,11 @@
-package com.example.adam.android_arduino_bluetooth;
+package com.ece3110.adam.android_arduino_bluetooth;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.CompoundButton;
 import android.widget.RadioGroup;
 import android.widget.Switch;
